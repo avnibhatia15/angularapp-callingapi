cfa2be020bee86a0b035b86671f18c2ad6eff1e8
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2022-08-29 02:19:42 +0530

    Getting the specific data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserResponse } from './UserResponse';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,10 @@ export class AppComponent {
   }//Injecting the client
   
   ngOnInit(): void{
-    this.http.get("https://api.github/users/abdullah").subscribe(data =>{ //Get pulls or extracts the data from the api{
-    console.log(data);
+    this.http.get<UserResponse>("https://api.github/users/abdullah").subscribe(data =>{ //Get pulls or extracts the data from the api{
+    console.log("User Login: "+ data.login);
+    console.log("Bio:"+ data.bio);
     });
-    //Calling the api using promise.
-    //Promise- If you want to make a asyncronous call(Having more than one line of code)
-    //When we call an api usimg a promise this call will be given to a slave(worker) thread the main thread will carry on executing the next lines of code.work of executing above code will be given to the given to the slave thread
-    //Once it is completed the task it will come to the master(main) thread and say im done.
-    //we convert something to promise To work with it asynchronously. 
+    //Only extracting what i want
   }
   }
